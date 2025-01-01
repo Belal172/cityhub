be9b44275bfda181fe4be88a29dcb83a7c45dbf0
Milan Belal <mohitbelal9@gmail.com>
2025-01-01 21:28:40 +0545

    second commit

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -4,7 +4,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=menu" />
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@24,400,0,0" />
+<link rel="stylesheet" href="css/adminstyle.css">
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,9 +17,47 @@
          <aside>
             <div class="top">
                 <div class="logo">
-                    
+                    <h3>Milan</h3>
                 </div>
             </div>
+            <!-- top end -->
+             <div class="sidebar">
+                <a href="" class="active">
+                <span class="material-symbols-sharp">home </span>
+                <h3>Home</h3>
+                </a>
+
+                <a href="">
+                <span class="material-symbols-sharp">celebration </span>
+                <h3>Events</h3>
+                </a>
+
+                <a href="">
+                <span class="material-symbols-sharp">reminder </span>
+                <h3>Notices</h3>
+                </a>
+
+                <a href="">
+                <span class="material-symbols-sharp">ballot </span>
+                <h3>Polls</h3>
+                </a>
+
+                <a href="">
+                <span class="material-symbols-sharp">cleaning_services </span>
+                <h3>Waste schedule</h3>
+                </a>
+
+                <a href="">
+                <span class="material-symbols-sharp">settings </span>
+                <h3>settings</h3>
+                </a>
+
+                <a href="">
+                <span class="material-symbols-sharp">logout </span>
+                <h3>Logout</h3>
+                </a>
+
+             </div>
 
          </aside>
 
@@ -26,6 +65,7 @@
 
         <!-- main area start -->
          <main>
+            <h1>main</h1>
 
          </main>
 
