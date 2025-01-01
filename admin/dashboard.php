fe0f6f987bef4d5dcd0e5d44fb744fdbb919c10e
Milan Belal <mohitbelal9@gmail.com>
2025-01-01 13:50:30 +0545

    first commit

---
+++ admin/dashboard.php
@@ -0,0 +1,37 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=menu" />
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <!-- aside section start -->
+         <aside>
+            <div class="top">
+                <div class="logo">
+                    
+                </div>
+            </div>
+
+         </aside>
+
+        <!-- aside end -->
+
+        <!-- main area start -->
+         <main>
+
+         </main>
+
+          <!-- main area end -->
+
+    </div>
+    <!--  -->
+</body>
+</html>
